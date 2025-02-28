eb9fb23f068b8b22e33006d2a37a52eeb2817827
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-28 16:47:13 +0000

    uehehehehenehbebbwvjhvfwggwysysvetsgsoskskjsusuwhwhebshshshsbshhsbsshshshshshshhshshshs

--- brusher/Winding.cpp
+++ brusher/Winding.cpp
@@ -1,5 +1,42 @@
 #include "Winding.h"
 
+/*Winding Name List*/
 const char * WindingName(Winding * winding){
+               static char wBuff[2048];
+                   if( winding->q_eBrushWindingBegin == true ){
+                       for( winding; winding->prev, winding->current, winding->next; winding++ ){
+                               sprintf(wBuff, "Winding %i", winding->WindingNumpoints);
+                       }
 
+                   }
+    return wBuff;
 };
+
+/*Print Winding Points*/
+void PrintWindingPoints( Winding * winding ){
+          int g_nWindingId;
+                   for( g_nWindingId = 0; g_nWindingId >= 0; g_nWindingId++ ){
+                     printf("%i, %i, %i", winding->WindingNumpoints[0], winding->WindingNumpoints[1],
+                       winding->WindingNumpoints[2]);
+                   }
+};
+
+/*Memory Allocate Winding*/
+Winding * AllocWinding(){
+         Winding * winding = (Winding*)malloc(sizeof(*winding));
+         return winding = AllocWinding();
+};
+
+/*Check Winding Malloc*/
+void Winding_Malloc(Winding * winding){
+    if(winding->AllocWinding()){
+       winding = AllocWinding();
+    }
+
+    else{
+        *winding;
+           delete[] winding;
+    }
+
+ return winding->prev.Winding_Malloc(winding);
+}
--- brusher/Winding.h
+++ brusher/Winding.h
@@ -7,11 +7,21 @@ public:
 
     int WindingNumpoints[3];
 
+    Winding ** next;
+    Winding * current;
+    Winding &prev;
+
+    qboolean q_eBrushWindingBegin;
+
     //Winding stuff
     Winding * AllocWinding();
     Winding * WindingConcave(int points);
     Winding * WindingLst(Winding * w);
     Winding * MakeWinding_Concave(Winding * winding);
+
+    void Winding_Concave(Winding * winding, int conpoints);
+    void Winding_Malloc(Winding * winding);
+    void PrintWinding_List(Winding * winding);
 
     void WindingSize(Winding * winding){
         std::size_t wSize = sizeof(*winding);
